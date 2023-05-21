0e33e2456008457eefb9bcadc5aea601a2e95f35
arjuna1011 <jgboatright@att.net>
2023-05-20 20:18:10 -0400

    Displaying post on home, added static files, added templates, and media folder created

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    image = models.ImageField(null=True,blank=True, upload_to="images/")
+    
+    def __str__(self) -> str:
+        return self.title
+    
